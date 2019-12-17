5c0b376082a85a17629957cf4c3b6ba9f52be52f
Brandi <blubrandi0@gmail.com>
2019-12-17 14:42:17 -0700

    Created XCode Project

---
+++ iOS Firebase Chat/iOS Firebase Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Firebase Chat
+//
+//  Created by Brandi Bailey on 12/17/19.
+//  Copyright © 2019 Brandi Bailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
